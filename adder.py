2eed40cc19d158dbfd6508bba4d8796b8d204bca
William S Chen <blackjamx5@hotmail.com>
2018-06-16 15:05:17 -0700

    Word Adder Script

---
+++ adder.py
@@ -0,0 +1,15 @@
+import csv
+from .models import entry
+
+
+with open('words.csv') as words:
+    reader = csv.reader(words)
+    for row in reader:
+        entry.objects.create(english=row[0], bhutia=row[1], tibetan=row[2], category=row[3])
+
+
+
+
+
+
+
