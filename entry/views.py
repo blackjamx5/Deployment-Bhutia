60da31c5af2a8e79a38b60d699a25d90806a02e8
William S Chen <blackjamx5@hotmail.com>
2018-07-11 20:50:10 -0700

    all

--- entry/views.py
+++ entry/views.py
@@ -2,10 +2,6 @@ from django.shortcuts import render
 from django.utils import timezone
 from .models import entry
 from django.http import HttpResponse
-from django.contrib.postgres.search import SearchQuery, SearchVector, SearchRank
-from django.conf.urls import include
-from django.conf.urls import url
-from django.http import HttpRequest, HttpResponseRedirect as redirect
 
 
 # Create your views here.
@@ -13,47 +9,29 @@ from django.http import HttpRequest, HttpResponseRedirect as redirect
 def home(request):
     return render(request, 'index.html')
 
-'''
 def search(request):
-    error = False
-    if 'q' in request.GET:
+    if 'q' in request.GET and request.GET['q']:
         q = request.GET['q']
-        if not q:
-            error = True
+        entrys = entry.objects.filter(bhutia__icontains=q)
+        if entrys:
+            return render(request, 'entry/entry_list.html',
+                          {'entrys': entrys, 'query': q})
         else:
-            exact_entry = entry.objects.filter(bhutia__iexact=q)
-            entrys = entry.objects.filter(bhutia__icontains=q)
-
-            #run comparisons
-
-            return render(request,  'entry/entry_list.html', {'entrys': entrys, 'query': exact_entry})
-        return render(request, 'entry/entry_list.html', {'error': error})
-'''
-
-def search(request):
-    error = False
-    if 'q' in request.GET:
-        q = request.GET['q']
-
-        if not q:
             error = True
-        else:
-            if request.path == "/entry/bhutia_english/":
-                exact_entry = entry.objects.filter(bhutia__iexact=q)
-                entrys = entry.objects.filter(bhutia__icontains=q)
-                return render(request, 'entry/entry_list.html', {'entrys': entrys, 'query': exact_entry})
+            return render(request, 'entry/entry_list.html', {'error': error})
 
-            if request.path == "/entry/english_bhutia/":
-                exact_entry = entry.objects.filter(english__iexact=q)
-                entrys = entry.objects.filter(english__icontains=q)
-                return render(request, 'entry/entry_list.html', {'entrys': entrys, 'query': exact_entry})
-
-            if request.path == "/entry/tibetan_bhutia/":
-                exact_entry = entry.objects.filter(tibetan__iexact=q)
-                entrys = entry.objects.filter(tibetan__icontains=q)
-                return render(request, 'entry/entry_list.html', {'entrys': entrys, 'query': exact_entry})
+    else:
+        error = True
         return render(request, 'entry/entry_list.html', {'error': error})
-
-def autocorrect(q):
-    
-    return
+#
+#
+# def search(request):
+#     error = False
+#     if 'q' in request.GET:
+#         q = request.GET['q']
+#         if not q:
+#             error = True
+#         else:
+#             books = entry.objects.filter(bhutia__icontains=q)
+#             return render(request, 'entry/entry_list.html', {'books': books, 'query': q})
+#     return render(request, 'index.html', {'error': error})
