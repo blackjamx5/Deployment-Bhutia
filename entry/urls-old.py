eaf5aba16946b7af9b9fb55de5df7f7f38a7cce6
William S Chen <blackjamx5@hotmail.com>
2018-07-11 21:44:31 -0700

    fuck

--- entry/urls-old.py
+++
@@ -1,9 +0,0 @@
-from django.conf.urls import url
-from django.urls import path
-from . import views
-
-urlpatterns = [
-    url(r'^$', views.home, name="home"),
-    url("entry/", views.search, name="entry"),
-
-]
